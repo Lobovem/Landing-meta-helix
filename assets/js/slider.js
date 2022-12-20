afd04e71cb5cb290d1f9449410eaf720b66bd9c4
Lobov Evgeniy <lobov.em@gmail.com>
2022-12-20 15:22:44 +0100

    Подлечил и сделал второй слайдер

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -19,3 +19,26 @@ const swiper = new Swiper('.swiper', {
   //   el: '.swiper-scrollbar',
   // },
 });
+
+const swipers = new Swiper('.swiper-features', {
+  spaceBetween: 30,
+  // Optional parameters
+  // direction: 'vertical',
+  // loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
