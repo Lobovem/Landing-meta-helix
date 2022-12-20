873ba76f87e463b84b5f0703054c748e487b1547
Lobov Evgeniy <lobov.em@gmail.com>
2022-12-20 18:52:13 +0100

    I maked scroll fot header-link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,60 @@
-// управление меню
-const menuBtn = document.querySelector('.navbar__burger');
-const menuList = document.querySelector('.navbar__list');
-const header = document.querySelector('header');
-const body = document.querySelector('body');
+'use strict';
 
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  IOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.IOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
 
-menuBtn.addEventListener('click', (e) => {
-  menuBtn.classList.toggle('active');
-  header.classList.toggle('active');
-  body.classList.toggle('active');
+if (isMobile.any()) {
+  document.body.classList.add('touch');
+} else {
+  document.body.classList.add('pc');
+}
 
-  if (menuList.classList.contains('active')) {
-    setTimeout(() => menuList.classList.toggle('active'), 500);
-  } else {
-    menuList.classList.toggle('active');
+//Прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.navbar__link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLinks => {
+    menuLinks.addEventListener('click', onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+      e.preventDefault();
+    }
   }
-});
-
-
-// считали DOM элементы
-const headersList = document.querySelectorAll('.slider');
-const indicatorsList = document.querySelectorAll('.slider__indicator');
-
-let index = 0;
-let interval = 5000;
-let heightsArr = [];
-let heightMax = null;
-
-setInterval(() => {
-  // снимаем классы active для первых элементов
-  headersList[index].classList.toggle('active');
-  indicatorsList[index].classList.toggle('active');
-  // увеличиваем индекс, пока не превышено количество элементов
-  index = (index + 1) % headersList.length;
-  // ставим классы active следующим элементам
-  headersList[index].classList.toggle('active');
-  indicatorsList[index].classList.toggle('active');
-}, interval);
-
-
-// Горизонтальный слайдер. Считали DOM элементы
-const headersLis = document.querySelectorAll('.slider-alt');
-const indicatorsLis = document.querySelectorAll('.slider-alt__indicator-alt');
-
-let ind = 0;
-let interv = 5000;
-let heightsAr = [];
-let heightM = null;
-
-setInterval(() => {
-  // снимаем классы active для первых элементов
-  headersLis[ind].classList.toggle('active');
-  indicatorsLis[ind].classList.toggle('active');
-  // увеличиваем индекс, пока не превышено количество элементов
-  ind = (ind + 1) % headersLis.length;
-  // ставим классы active следующим элементам
-  headersLis[ind].classList.toggle('active');
-  indicatorsLis[ind].classList.toggle('active');
-}, interval);
+}
