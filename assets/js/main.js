1a8a2c57f7de3d1664a4a93d2dfc72fd1da5a7c9
Lobov Evgeniy <lobov.em@gmail.com>
2023-05-31 16:28:12 +0200

    Added remove active class

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,13 +17,7 @@ const isMobile = {
     return navigator.userAgent.match(/IEMobile/i);
   },
   any: function () {
-    return (
-      isMobile.Android() ||
-      isMobile.BlackBerry() ||
-      isMobile.IOS() ||
-      isMobile.Opera() ||
-      isMobile.Windows()
-    );
+    return isMobile.Android() || isMobile.BlackBerry() || isMobile.IOS() || isMobile.Opera() || isMobile.Windows();
   },
 };
 
@@ -33,17 +27,24 @@ if (isMobile.any()) {
   document.body.classList.add('pc');
 }
 
-
 //Меню бургер
 
 const burgerMenu = document.querySelector('.navbar__burger');
 if (burgerMenu) {
   const navbarList = document.querySelector('.navbar__list');
-  burgerMenu.addEventListener("click", function (e) {
-    //Когда открылось меню, чтобы не было скролла страницы
-    document.body.classList.toggle('lock');
+  burgerMenu.addEventListener('click', (e) => {
+    document.body.classList.toggle('lock'); //Когда открылось меню, чтобы не было скролла страницы
     burgerMenu.classList.toggle('active');
     navbarList.classList.toggle('active');
+  });
+
+  const navbarLink = document.querySelectorAll('.navbar__link');
+  navbarLink.forEach((navbarLink) => {
+    navbarLink.addEventListener('click', (e) => {
+      document.body.classList.remove('lock'); //Когда открылось меню, чтобы не было скролла страницы
+      burgerMenu.classList.remove('active');
+      navbarList.classList.remove('active');
+    });
   });
 }
 
@@ -51,19 +52,16 @@ if (burgerMenu) {
 
 const menuLinks = document.querySelectorAll('.navbar__link[data-goto]');
 if (menuLinks.length > 0) {
-  menuLinks.forEach(menuLinks => {
+  menuLinks.forEach((menuLinks) => {
     menuLinks.addEventListener('click', onMenuLinkClick);
   });
 
   function onMenuLinkClick(e) {
     const menuLink = e.target;
-    if (
-      menuLink.dataset.goto &&
-      document.querySelector(menuLink.dataset.goto)) {
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
       const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      const gotoBlockValue =
-        gotoBlock.getBoundingClientRect().top;
-      //  Если нужно вычесть высоту блока, например position:fixed 
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top;
+      //  Если нужно вычесть высоту блока, например position:fixed
       // + pageXOffset -
       // document.querySelector('.test').offsetHeight;
 
